5580215630ebafb8f610836fbdaa28a2439d0dcc
Felopater Melika <felopatermelika@gmail.com>
2024-10-08 17:02:58 -0500

    background

--- app.vue
+++ app.vue
@@ -1,7 +1,21 @@
 <template>
   <NuxtLayout>
-    <v-app>
-      <NuxtPage />
-    </v-app>
+    <div class="relative min-h-screen w-full">
+      <!-- Blurry background image -->
+      <!-- Disable ESLint and Prettier for the image URL -->
+      <!-- eslint-disable-next-line -->
+      <!-- prettier-ignore -->
+      <div
+          class="absolute inset-0 w-full h-full bg-cover bg-center bg-no-repeat blur-sm opacity-60"
+          style="background-image: url('/IMG_0943[1].jpg');"
+      ></div>
+
+      <!-- Main content -->
+      <div class="relative z-10">
+        <v-app>
+          <NuxtPage />
+        </v-app>
+      </div>
+    </div>
   </NuxtLayout>
 </template>
